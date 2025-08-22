cfe13de3980bea88e4024c716c247f76518aa2d6
Kalkidan404 <kalrokal1621@gmail.com>
2025-08-22 13:51:23 +0300

    update

--- poem.js
+++ poem.js
@@ -2,6 +2,7 @@ let poemElement = document.querySelector(".poem");
 let submitbttn = document.querySelector(".input-bttn");
 let searchinput = document.querySelector(".search-input");
 let title = document.querySelector(".poem-input");
+let process = document.querySelector(".process");
 function generator(event) {
   event.preventDefault();
 
@@ -10,7 +11,8 @@ function generator(event) {
 }
 
 function poempro(prompt) {
-  let context = "give me a short poem using the prompt as a title";
+  let context =
+    "give me a short poem using the prompt as a title with each lines separated the right way and every time asked the same topic generate a neew poem for it";
 
   let apikey = "de072a3do70f178f6a0bt43f94a33afc";
   let apiurl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apikey}`;
@@ -18,6 +20,7 @@ function poempro(prompt) {
   axios.get(apiurl).then(handleresponse);
 }
 function handleresponse(response) {
+  process.innerHTML = `<div class="blink">⏳generating a poem about ${searchinput.value}</div> `;
   poemElement.innerHTML = response.data.answer;
 
   new Typewriter(".poem", {
