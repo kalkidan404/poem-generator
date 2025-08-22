420073e9f5b9184a06465fae4b125e4103989df7
Kalkidan404 <kalrokal1621@gmail.com>
2025-08-22 11:45:16 +0300

    updates

--- poem.js
+++ poem.js
@@ -1,5 +1,29 @@
-let h1 = document.querySelector(".welcome");
-function welcome() {
-  alert("hello");
+let poemElement = document.querySelector(".poem");
+let submitbttn = document.querySelector(".input-bttn");
+let searchinput = document.querySelector(".search-input");
+let title = document.querySelector(".poem-input");
+function generator(event) {
+  event.preventDefault();
+  new Typewriter(".poem", {
+    strings:
+      "The tree that never had to fight For sun and sky and air and light",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+  let prompt = searchinput.value;
+  poempro(prompt);
 }
-welcome();
+
+function poempro(prompt) {
+  let context = "";
+
+  let apikey = "";
+  let apiurl = "";
+
+  axios.get(apiurl).then(handleresponse);
+}
+function handleresponse(response) {
+  poemElement.innerHTML = response.data.answer;
+}
+title.addEventListener("submit", generator);
